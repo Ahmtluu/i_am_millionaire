478e8b8e79f62e162f32584dc0b36593b6bc50ab
Ahmtluu <ahmtt.mtlu@gmail.com>
2021-03-27 16:31:40 +0300

    first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(I_am_millionaire());
+}
+
+
+class I_am_millionaire extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.teal,
+              title: Center(child: Text("I Am Millionaire !")),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                      "images/money.svg",
+                    width: 250,
+                  ),
+                  SizedBox(height: 10,),
+                  Text("YEAH! You know I'm millionaire!",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontFamily:"Source Sans Pro",
+                      color: Colors.teal[500]
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ),
+          ),
+    );
+  }
+}
+
+
